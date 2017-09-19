838e7173090292126c1eed6affe7b75a01a3c447
ZTY <saiting@126.com>
2017-09-19 10:27:04 +0800

    checkout

--- MyFEM/MyFEM3d/NmatHeat2D_triangular.m
+++
@@ -1,4 +0,0 @@
-% Shape function  
-function N = NmatHeat2D_triangular(eta,psi) 
-        
-N  =  [1-psi-eta  eta  psi]; 
--- MyFEM/MyFEM3d/NmatHeat3D.m
+++
@@ -1,5 +0,0 @@
-% Shape function  
-function N = NmatHeat3D(eta,psi,omega) 
-        
-N  = 0.125 * [(1-psi)*(1-eta)*(1-omega)  (1+psi)*(1-eta)*(1-omega)  (1+psi)*(1+eta)*(1-omega)  (1-psi)*(1+eta)*(1-omega) ...
-              (1-psi)*(1-eta)*(1+omega)  (1+psi)*(1-eta)*(1+omega)  (1+psi)*(1+eta)*(1+omega)  (1-psi)*(1+eta)*(1+omega)]; 
